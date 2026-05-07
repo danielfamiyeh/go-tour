a40168e4d5120821319db808092057c541aa782b
Daniel Famiyeh <daniel.famiyeh@altium.com>
2026-05-07 15:18:17 +0100

    feat(list): Add AddLeaf method

--- list.go
+++ list.go
@@ -23,9 +23,26 @@ func (l *List[T]) String() string {
 	return str
 }
 
+func (l *List[T]) Leaf() *List[T] {
+  curr := l;
+
+  for curr.next != nil { 
+  	curr = curr.next
+	}
+
+	return curr
+}
+
+func (l *List[T]) AddLeaf(val T) {
+	leaf := l.Leaf()
+	leaf.next = &List[T]{ val: val, next: nil }
+}
 
 func main() {
 	l := &List[int] { val: 1, next: &List[int] { val: 3, next: &List[int] { val: 5, next: nil }}}
+	fmt.Println(l)
+
+	l.AddLeaf(7)
 	fmt.Println(l)
 }
 
